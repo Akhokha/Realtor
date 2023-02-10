c417139be9af890ea04318944e485eecfd909c1f
Soma <mac@Macs-MacBook-Pro.local>
2023-02-10 17:13:53 +0200

    Install ReactJS, Tailwind css, Create the first template

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+module.exports = {
+  mode: "jit", //just in time compiler
+  content: [
+    // Example content paths...
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  // darkMode: media, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      body: ["Montserrat", "sans-serif"],
+    }, // provide font family.
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("tailwind-scrollbar-hide"),
+    // ...
+  ],
+};
